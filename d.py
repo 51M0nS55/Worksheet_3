1070694916728cdd9132b0fa3575bc66e08d825c
51M0nS55 <yilang@ucsb.edu>
2025-02-12 02:13:33 +0000

    Initial commit: Add Worksheet 3 files

---
+++ d.py
@@ -0,0 +1,12 @@
+import random
+from matplotlib import pyplot as plt
+
+from abc import steps_count
+
+random.seed(1108)
+
+for a, b in [(2,3), (3,2), (3,5), (5,3), (3,12), (12,3)]:
+	plt.hist(steps_count(a, b))
+	plt.title(f'$L_{{{a},{b}}}$')
+	plt.xlabel('Steps')
+	plt.show()
